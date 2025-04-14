94863a80f7731f1cad418b0fb4dcf87bf088de7a
Kesh <59233228+Kezia-Cane@users.noreply.github.com>
2025-04-14 22:52:56 +0800

    installed Mongoose dependency

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
